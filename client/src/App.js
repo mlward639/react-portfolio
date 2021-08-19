d9affd376fd2522788d8b4f45c54e9b4bcfb2c3c
Maggie Ward <mlward639@gmail.com>
2021-08-19 00:27:46 -0400

    AboutMe component added with link in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
 
 /* ?? NEEDED?? 
 const client = new ApolloClient({
---
+++ client/src/components/AboutMe/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className="row about-me" id="about-me">
+      <div className="col-2 col left-titles">
+        <h2>About Me</h2>
+      </div>
+      <div className="col-7 col about-me-description">
+        Dedicated and hard-working professional making the move to web
+        development from the medical field. Currently enrolled in Georgia Tech
+        Coding Boot Camp and set to graduate at the end of August 2021.
+        Curriculum includes HTML5, CSS3, JavaScript, jQuery, Progressive Web
+        Apps, Agile Methodology, Bootstrap, React.js, Express.js, Node.js,
+        MongoDB, MySQL, Command Line, and Git. Strong academic foundation,
+        goal-oriented and self-motivated. Proven skills in leadership, critical
+        thinking, and working with a multidisciplinary team towards a common
+        goal.
+      </div>
+      <div className="col-3 col">IMAGE HERE</div>
+    </div>
+  );
+};
+
+export default Footer;
