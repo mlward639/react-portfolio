5c6c01746fd60b6ef5a3db883823dc06a07e0bc4
Maggie Ward <mlward639@gmail.com>
2021-08-20 18:42:28 -0400

    work cards array added to minorworkcards.js

--- client/src/components/PreviousWork/minorworkcards.js
+++ client/src/components/PreviousWork/minorworkcards.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const minorWorkCards = [
+  { id: 1, WorkDeployedURL: "https://mlward639.github.io/Project-One-Sassy-Pets/", WorkImgSrc: "client/public/images/project-one-screenshot-deployed-url.png", WorkImgAlt: "Screenshot of Sassy Pets", WorkTitle: "Sassy Pet Adoption Application", WorkTech: "HTML, CSS, JS", WorkGithubRepo: "https://github.com/mlward639/Project-One-Sassy-Pets" },
+  { id: 2, WorkDeployedURL: "https://mlward639.github.io/unit-six-homework/", WorkImgSrc: "client/public/images/weather-screenshot.png", WorkImgAlt: "weather-screenshot", WorkTitle: "Weather Dashboard", WorkTech: "HTML, CSS, JS", WorkGithubRepo: "https://github.com/mlward639/unit-six-homework" }
+  { id: 3, WorkDeployedURL: "", WorkImgSrc: "", WorkImgAlt: "", WorkTitle: "", WorkTech: "", WorkGithubRepo: "" }
+  { id: 4, WorkDeployedURL: "", WorkImgSrc: "", WorkImgAlt: "", WorkTitle: "", WorkTech: "", WorkGithubRepo: "" }
+  { id: 5, WorkDeployedURL: "#", WorkImgSrc: "client/public/images/placeholder-image.jpg", WorkImgAlt: "#", WorkTitle: "Project 3 Placeholder", WorkTech: "Placeholder", WorkGithubRepo: "#" }
+  
+
+]
+
 const MinorWork = () => {
   return (
     <div className="col-5 col">
@@ -7,10 +17,10 @@ const MinorWork = () => {
         <imgPath
           src={WorkImgSrc}
           alt={WorkImgAlt}
-          className="work-pic {`work-pic${Work.id}`}" //should look like this: work-pic1
+          className="work-pic {`work-pic${minorWorkCards.id}`}" //should look like this: work-pic1
         />
       </a>
-      <div className="other-work-description {`other-issue${Work.id}`}">
+      <div className="other-work-description {`other-issue${minorWorkCards.id}`}">
         {" "}
         //should look like this: other-issue1
         <h3>{WorkTitle}</h3>
@@ -25,4 +35,4 @@ const MinorWork = () => {
   );
 };
 
-export default MinorWork;
+export default { MinorWork, minorWorkCards };
